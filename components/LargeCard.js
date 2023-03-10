205e8d9d624cb892485282285b16ac4acce33451
Philippe Brouillet <philippebrouillet@icloud.com>
2023-03-10 17:50:49 +0100

    modified:   components/LargeCard.js

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -5,7 +5,7 @@ function LargeCard({ img, title, description, buttonText }) {
   return (
     <div className="relative py-16 cursor-pointer">
       <div className="relative h-96 min-w-[300px]">
-        <Image
+        <img
           alt="imgbigcard"
           src={img}
           layout="fill"
